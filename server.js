4741fcb62922baeac0f9839cac9e8406ea690575
Charles C <charles.coulais@marcelww.com>
2016-01-23 18:56:44 +0100

    final commit

--- server.js
+++ server.js
@@ -1,27 +1,89 @@
 var fs = require('fs');
-var html = fs.readFileSync(__dirname+'/index.html');
-var app = require('http').createServer(function(req, res){ res.end(html); });
+var app = require('http').createServer(function(req, res){
+
+    var html = fs.readFileSync(__dirname+'/index.html');
+    res.end(html);
+
+});
+
+
 
 app.listen(8080);
 
 var io = require("socket.io");
 var io = io.listen(app);
 
-var uid = (new Date()).getTime(),
-    lastId = -1,
+var lastId = -1,
     list = {};
 
-io.sockets.on('connection', function (socket) {
-    socket.emit('faitUneAlerte', {
-        'uid' : 666,
-        'piList' : list
-    });
-});
-io.sockets.on('new', function (socket) {
-    ++lastId;
 
-    socket.emit('new_confirm', {
-        'uid' : 666,
-        'piList' : []
+
+io.sockets
+    .on('connection', function (socket) {
+        console.log('new user');
+
+        var uid = (new Date()).getTime();
+
+        socket.emit('initApp', {
+            'uid' : uid,
+            'list' : list
+        });
+
+
+        socket.on('new_pi', function () {
+            console.log('new PI');
+
+            ++lastId;
+
+            var pi = list[lastId] = {
+                'id' : lastId,
+                'title' : '',
+                'desc' : '',
+                'editor' : uid
+            };
+
+            socket.emit('new_pi', {
+                'uid' : uid,
+                'pi' : pi
+            });
+
+            socket.broadcast.emit('new_pi', {
+                'uid' : uid,
+                'pi' : pi
+            });
+        });
+        socket.on('edit_pi', function (obj) {
+            console.log('edit PI');
+
+
+            var pi = list[obj.pid];
+            pi.editor = uid;
+
+            socket.emit('edit_pi', {
+                'uid' : uid,
+                'pid' : pid
+            });
+
+            socket.broadcast.emit('edit_pi', {
+                'uid' : uid,
+                'pid' : pid
+            });
+        });
+        socket.on('update_pi', function (obj) {
+            console.log('update PI');
+
+
+            var pi = list[obj.pid];
+            pi.editor = null;
+
+            socket.emit('edit_pi', {
+                'uid' : uid,
+                'pid' : pid
+            });
+
+            socket.broadcast.emit('edit_pi', {
+                'uid' : uid,
+                'pid' : pid
+            });
+        });
     });
-});
